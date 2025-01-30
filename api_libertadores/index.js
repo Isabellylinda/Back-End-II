099c20f69d1cda27ab986edcb635a049a778a1d1
Isalinda <Isabelly.lbispo@gmail.com>
2025-01-30 11:40:07 -0400

    save

---
+++ api_libertadores/index.js
@@ -0,0 +1,25 @@
+// import pool from './servicos/conexao.js';
+import express from 'express';
+import { retornaCampeonatos, retornaCampeonatosID } from './servico/retornaCampeonatos_servico.js';
+
+const app = express();
+
+app.get('/campeonatos', async (req, res) => {
+    const campeonatos = await retornaCampeonatos();
+    res.json(campeonatos);
+})
+
+app.get('/campeonatos/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+    const campeonato = await retornaCampeonatosID(id);
+    res.json(campeonato);
+})
+
+app.listen(9000, async () => {
+    const data = new Date();
+    console.log("Servidor node iniciado em: " +data);
+
+    // const conexao = await pool.getConnection();
+    // console.log(conexao.threadId);
+    // conexao.release()
+})
