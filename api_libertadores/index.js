f65dbc0efc428a8ca1b47b1ed33b034ecd8e5d2e
Isalinda <Isabelly.lbispo@gmail.com>
2025-02-13 11:32:50 -0400

    sace

--- api_libertadores/index.js
+++ api_libertadores/index.js
@@ -5,7 +5,7 @@ import { retornaCampeonatosID } from './servico/retornaCampeonatos_servico.js';
 import { retornaCampeonatosAno} from './servico/retornaCampeonatos_servico.js';
 import { retornaCampeonatosTime} from './servico/retornaCampeonatos_servico.js';
 import { cadastroCampeonato } from './servico/cadastroCampeonato_servico.js';
-import { atualizaCampeonato } from './servico/atualizaCampeonato_servico.js';
+import { atualizaCampeonato, atualizaCampeonatoParcial } from './servico/atualizaCampeonato_servico.js';
 
 const app = express();
 app.use(cors());
@@ -15,6 +15,31 @@ app.use(express.json()); //Suporte para JSON no corpo da resiquisicao
 //      const campeonatos = await retornaCampeonatos();
 //    res.json(campeonatos)
 //  })
+
+app.patch('/campeonatos/:id', async(req, res) => {
+    const {id} = req.params;
+    const{campeao, vice, ano} = req.body;
+
+    const camposAtualizar = {};
+    if(campeao) camposAtualizar.campeao = campeao;
+    if(vice) camposAtualizar.vice = vice;
+    if(ano) camposAtualizar.ano = ano;
+
+    if (Object.keys(camposAtualizar).length === 0) {
+      res.status(400).send("Nenhum campo válido foi enviado para atualização")
+    } else {
+      const resultado = await atualizaCampeonatoParcial(id, camposAtualizar)
+      if (resultado.affectedRows > 0) {
+        res.status(202).send("Registro atualizado com sucesso")
+        
+      } else {
+        res.status(404).send("Registro não encontrado");
+        
+      }
+    }
+
+})
+
 
 app.put('/campeonatos/:id', async (req, res) =>{
   const{id} = req.params;
